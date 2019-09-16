e2a62f8b0eb6677922333edf00925d31ea930059
akm <akm2000@gmail.com>
2019-09-16 11:18:40 +0900

    :+1: Define AccountStore.ExistByToken

--- account/store/account_store.go
+++ account/store/account_store.go
@@ -21,14 +21,34 @@ func NewAccountStore(logger *log.Logger) *AccountStore {
 	return &AccountStore{logger: logger}
 }
 
+const AccountKind = "Account"
+
+func (s *AccountStore) Kind() string {
+	return AccountKind
+}
+
 func (s *AccountStore) Put(ctx context.Context, m *accountmodel.Account) (res datastore.Key, err error) {
 	err = ds.WithBoom(ctx, func(bm *boom.Boom) error {
 		key, err := bm.Put(m)
 		if err != nil {
-			s.logger.Printf("Failed to Put %v because of [%T] %v\n", m, err)
+			s.logger.Printf("Failed to Put %v because of [%T] %v\n", m, err, err)
 			return err
 		}
 		res = key
+		return nil
+	})
+	return
+}
+
+func (s *AccountStore) ExistByToken(ctx context.Context, token string) (res bool, err error) {
+	err = ds.WithBoom(ctx, func(bm *boom.Boom) error {
+		q := bm.NewQuery(s.Kind()).Filter("JwtToken =", token).Limit(1).KeysOnly()
+		keys, err := bm.GetAll(q, nil)
+		if err != nil {
+			s.logger.Printf("Failed to Get keys for %s because of [%T] %v\n", token, err, err)
+			return err
+		}
+		res = len(keys) > 0
 		return nil
 	})
 	return
