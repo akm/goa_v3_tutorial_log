23398011169165360cdc05c7ccea162884a2f8c1
akm <akm2000@gmail.com>
2019-09-23 10:56:08 +0900

    :+1: Implement accountsrvc.Signin to generate JWT token

--- account.go
+++ account.go
@@ -4,6 +4,9 @@ import (
 	account "calcsvc/gen/account"
 	"context"
 	"log"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
 // account service example implementation.
@@ -20,5 +23,20 @@ func NewAccount(logger *log.Logger) account.Service {
 // Creates a valid JWT
 func (s *accountsrvc) Signin(ctx context.Context, p *account.SigninPayload) (res string, err error) {
 	s.logger.Print("account.signin")
-	return
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"nbf":    time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
+		"iat":    time.Now().Unix(),
+		"scopes": []string{"api:read"}, // Doesn't include api:write
+	})
+
+	s.logger.Printf("user '%s' logged in", p.Username)
+
+	// note that if "SignedString" returns an error then it is returned as
+	// an internal error to the client
+	t, err := token.SignedString(Key)
+	if err != nil {
+		return "", err
+	}
+	return t, nil
 }
